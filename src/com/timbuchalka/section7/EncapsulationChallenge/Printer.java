c6c949ccd07564d2f6033e18985ac90d7202ffe3
Tóth László <tl0904@gmail.com>
2019-05-10 21:06:05 +0200

    Printer Class created

---
+++ src/com/timbuchalka/section7/EncapsulationChallenge/Printer.java
@@ -0,0 +1,31 @@
+package com.timbuchalka.section7.EncapsulationChallenge;
+
+public class Printer {
+    
+    private int tonerLevel;
+    private int printedPages;
+    private boolean duplexPrinter;
+
+    /**
+     * @return the tonerLevel
+     */
+    public int getTonerLevel() {
+        return tonerLevel;
+    }
+
+    /**
+     * @return the printedPages
+     */
+    public int getPrintedPages() {
+        return printedPages;
+    }
+
+    /**
+     * @return the duplexPrinter
+     */
+    public boolean isDuplexPrinter() {
+        return duplexPrinter;
+    }
+    
+    
+}
