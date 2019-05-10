ad985e98131dd2c909214dba4c458974a7119113
Tóth László <tl0904@gmail.com>
2019-05-10 21:31:47 +0200

    Constructor, printingPages, setToner created

--- src/com/timbuchalka/section7/EncapsulationChallenge/Printer.java
+++ src/com/timbuchalka/section7/EncapsulationChallenge/Printer.java
@@ -6,11 +6,20 @@ public class Printer {
     private int printedPages;
     private boolean duplexPrinter;
 
+    public Printer(boolean duplexPrinter) {
+        this.tonerLevel = 100;
+        this.duplexPrinter = duplexPrinter;
+    }
+
     /**
      * @return the tonerLevel
      */
     public int getTonerLevel() {
         return tonerLevel;
+    }
+
+    public void setTonerLevel() {
+        this.tonerLevel = 100;
     }
 
     /**
@@ -27,5 +36,16 @@ public class Printer {
         return duplexPrinter;
     }
     
-    
+    public void printingPage() {
+        if (duplexPrinter) {
+            this.printedPages += 2;
+            this.tonerLevel -= 2;
+        } else {
+            this.printedPages += 1;
+            this.tonerLevel -= 1;
+        }
+        if (tonerLevel <= 6) {
+            System.out.println("Toner is running out, bye a new one!");
+        }
+    }
 }
