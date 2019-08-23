adaac6b0f827cae5cd7dc401b59f69e955765690
Tóth László <tl0904@gmail.com>
2019-08-23 15:00:28 +0200

    In the Secton8 the playlist modified.

--- src/com/timbuchalka/section8/PlaylistForSongs/Main.java
+++ src/com/timbuchalka/section8/PlaylistForSongs/Main.java
@@ -17,10 +17,9 @@ public class Main {
     private static LinkedList<Song> playList = new LinkedList<>();
     private static ListIterator<Song> i;
     private static Song aktual;
-    
+
     public static void main(String[] args) {
-        
-        
+
         Album fireball = new Album("Deep Purple - Fireball");
         fireball.addNewSong("Fireball", "3:24");
         fireball.addNewSong("No No No", "6:54");
@@ -31,7 +30,7 @@ public class Main {
         fireball.addNewSong("No One Came", "6:34");
         fireball.addNewSong("Strange Kind Of Woman", "4:07");
         albums.add(fireball);
-        
+
         Album schoolDays = new Album("Stanley Clarke - School Days");
         schoolDays.addNewSong("School Days", "7:51");
         schoolDays.addNewSong("Quiet Afternoon", "5:10");
@@ -40,7 +39,7 @@ public class Main {
         schoolDays.addNewSong("Hot Fun", "2:55");
         schoolDays.addNewSong("Life Is Just A Game", "8:59");
         albums.add(schoolDays);
-        
+
         Album chariots = new Album("Vangelis - Chariots of Fire");
         chariots.addNewSong("Chariots of Fire (\"Titles\")", "3:33");
         chariots.addNewSong("Five Circles", "5:20");
@@ -50,7 +49,7 @@ public class Main {
         chariots.addNewSong("Jerusalem", "2:47");
         chariots.addNewSong("Chariots of Fire", "20:41");
         albums.add(chariots);
-        
+
         boolean quit = false;
         int choise = 0;
 //        printInstructions();
@@ -59,7 +58,7 @@ public class Main {
             System.out.println("Enter your choise!");
             choise = sc.nextInt();
             sc.nextLine();
-            
+
             switch (choise) {
                 case 0:
                     quit = true;
@@ -72,28 +71,20 @@ public class Main {
                     System.out.println("Please enter the song number");
                     int songNumber = sc.nextInt();
                     sc.nextLine();
-                    playList.add(new Song(albums.get(albumNumber - 1).getAlbum().get(songNumber - 1).getTitle(), 
+                    playList.add(new Song(albums.get(albumNumber - 1).getAlbum().get(songNumber - 1).getTitle(),
                             albums.get(albumNumber - 1).getAlbum().get(songNumber - 1).getDuration()));
                     break;
                 case 2:
                     printPlayList();
                     break;
                 case 3:
-                    skip();
+                    skip(0);
                     break;
                 case 4:
-                    System.out.println("Please enter the existing name:");
-                    String existingName = sc.nextLine();
-                    System.out.println("Please enter new contact name:");
-                    String newName = sc.nextLine();
-                    System.out.println("Please enter new phone number:");
-                    String newNumber = sc.nextLine();
-
+                    skip(1);
                     break;
                 case 5:
-                    System.out.print("Please enter the name to remove: ");
-                    String nameToRemove = sc.nextLine();
-
+                    skip(2);
                     break;
                 case 6:
                     System.out.print("Please enter the name to search: ");
@@ -104,20 +95,21 @@ public class Main {
                     throw new AssertionError();
             }
         }
-        
+
     }
 
     private static void printAlbums() {
         for (int i = 0; i < albums.size(); i++) {
             System.out.println((i + 1) + " " + albums.get(i).getName());
             for (int j = 0; j < albums.get(i).getAlbum().size(); j++) {
-                System.out.println("\t" + (j + 1) + " " + albums.get(i).getAlbum().get(j).getTitle() 
+                System.out.println("\t" + (j + 1) + " " + albums.get(i).getAlbum().get(j).getTitle()
                         + " " + albums.get(i).getAlbum().get(j).getDuration());
             }
             System.out.println("------------------------------------------------");
 
         }
     }
+
     private static void printInstructions() {
         System.out.println("\nPress!");
         System.out.println("\t 0 - To quit the application");
@@ -128,7 +120,7 @@ public class Main {
         System.out.println("\t 5 - To replay the current song");
         System.out.println("\t 6 - To remove song from the playlist");
     }
-    
+
     private static void printPlayList() {
         Iterator<Song> i = playList.iterator();
         while (i.hasNext()) {
@@ -137,23 +129,43 @@ public class Main {
         }
     }
 
-
-    private static void skip() {
+    private static void skip(int number) {
         i = playList.listIterator();
-        aktual = playList.getFirst();
-        while (i.hasNext()) {
-            Song listSong = i.next();
-            int comparison = listSong.getTitle().compareTo(aktual.getTitle());
-            if (comparison == 0) {
-                System.out.println("Now playing: " + listSong.getTitle() + " " + listSong.getDuration());
-                aktual = listSong;
-                break;
-            } else if (comparison > 0) {
-                System.out.println("nagyobb");
-            } else if (comparison < 0) {
-                System.out.println("kisebb");
+//        aktual = playList.getFirst();
+        boolean quit = false;
+
+        if (playList.isEmpty()) {
+            System.out.println("There's no song in the playlist.");
+            return;
+        } else {
+            while (!quit) {
+//            int comparison = listSong.getTitle().compareTo(aktual.getTitle());
+                switch (number) {
+                    case 0:
+                        Song listSong = i.next();
+                        System.out.println("Now playing: " + i.next().getTitle());
+//                        aktual = i.next();
+                        quit = true;
+                        break;
+                    case 1:
+//                        listSong = i.previous();
+                        if (i.hasPrevious()) {
+                            System.out.println("Now playing: " + i.previous().getTitle());
+//                        aktual = listSong;
+                        } else {
+                            System.out.println("This is the begin of the playlist");
+                        }
+                        quit = true;
+                        break;
+                    case 2:
+//                        listSong = i.next();
+                        System.out.println("Now playing: " + i.next().getTitle());
+//                        aktual = listSong;
+                        quit = true;
+                        break;
+                }
             }
         }
     }
-    
+
 }
